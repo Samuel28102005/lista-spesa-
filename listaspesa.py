644dfdb4e723e1c22e1fd6b5218ff43b178acc0e
Samuel28102005 <labarba.samuel@iisgalvanimi.edu.it>
2024-11-14 07:53:52 +0000

    remove

--- listaspesa.py
+++ listaspesa.py
@@ -3,6 +3,23 @@ lista = []
 def riempi_lista(lista):
     prodotto = input("inserire prodotto:")
     lista.append(prodotto)
-    print(lista)
 
 riempi_lista(lista)
+riempi_lista(lista)
+riempi_lista(lista)
+
+
+def Stampa_lista(lista):
+    print("lista della spesa")
+    for i in range(len(lista)):
+        print(f"{i + 1}. {lista[i]}")
+
+
+def rimuovi_lista(lista):
+    prodotto = input ("inserisci il prodotto da rimuovere dalla lista:")
+    lista.pop(lista.index(prodotto))
+
+rimuovi_lista(lista)
+Stampa_lista(lista)
+
+
